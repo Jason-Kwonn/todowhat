85aaa995b1965574fd946319b777a2878c8e63d6
Jason Kwon <jasonproject628@gmail.com>
2023-05-07 10:47:31 +0900

    Initial commit

---
+++ src/main/java/com/tdw/todowhat/TodowhatApplication.java
@@ -0,0 +1,13 @@
+package com.tdw.todowhat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodowhatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodowhatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdw/todowhat/TodowhatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdw.todowhat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodowhatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
